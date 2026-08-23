8f458d0fb4402d288c7ad1cf052764c8fc96294a
Sebastian Bermudez <sebastianbv2000@gmail.com>
2026-08-23 13:07:57 -0500

    Agregar función de resta y ampliar pruebas

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 def sumar(numero1, numero2):
     return numero1 + numero2
+
+
+def restar(numero1, numero2):
+    return numero1 - numero2
--- test_app.py
+++ test_app.py
@@ -1,5 +1,17 @@
-from app import sumar
+from app import sumar, restar
 
 
 def test_sumar():
     assert sumar(2, 3) == 5
+
+
+def test_sumar_negativos():
+    assert sumar(-2, -3) == -5
+
+
+def test_restar():
+    assert restar(5, 3) == 2
+
+
+def test_restar_resultado_negativo():
+    assert restar(3, 5) == -2
