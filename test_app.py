fb1fe825c2d8564240e74eda2984b3fbc1e91422
Sebastian Bermudez <sebastianbv2000@gmail.com>
2026-08-23 21:48:51 -0500

    Crear API Flask y agregar pruebas de endpoints

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from app import sumar, restar
+from app import app, sumar, restar
 
 
 def test_sumar():
@@ -15,3 +15,33 @@ def test_restar():
 
 def test_restar_resultado_negativo():
     assert restar(3, 5) == -2
+
+
+def test_inicio():
+    cliente = app.test_client()
+
+    respuesta = cliente.get("/")
+
+    assert respuesta.status_code == 200
+    assert respuesta.get_json()["mensaje"] == "API CI/CD funcionando"
+    assert respuesta.get_json()["estado"] == "ok"
+
+
+def test_sumar_api():
+    cliente = app.test_client()
+
+    respuesta = cliente.get("/sumar/2/3")
+
+    assert respuesta.status_code == 200
+    assert respuesta.get_json()["numero1"] == 2
+    assert respuesta.get_json()["numero2"] == 3
+    assert respuesta.get_json()["resultado"] == 5
+
+
+def test_salud():
+    cliente = app.test_client()
+
+    respuesta = cliente.get("/salud")
+
+    assert respuesta.status_code == 200
+    assert respuesta.get_json()["estado"] == "saludable"
